17fa34b41f877d72c35cbee5bc65fd05e31e79e5
alishowsilabas <alissonsilabas@gmail.com>
2023-09-13 21:02:23 -0300

    calculator update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function outcome(){
+function outcome() {
     let numOne = Number(document.getElementById('num-one').value)
     let numTwo = Number(document.getElementById('num-two').value)
     let total = 0
@@ -13,4 +13,32 @@ function outcome(){
         total = numOne / numTwo
     document.getElementById('resultArea').innerHTML = 'Result: ' + String(total)
     return total
+}
+
+function button(event) {
+    const value = event.target.innerHTML;
+    const expression = document.getElementById("expression");
+    const currentValue = expression.value;
+    const result = document.getElementById("result");
+
+    if (result.value.length > 0) {
+        
+    }
+
+    expression.value = currentValue + value
+}
+
+function result() {
+    const expression = document.getElementById("expression").value;
+    const result = document.getElementById("result");
+
+    result.value = eval(expression)
+}
+
+function clear() {
+    const expression = document.getElementById("expression");
+    const result = document.getElementById("result");
+    
+    expression.value = "";
+    result.value = "";
 }
